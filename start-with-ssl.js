e9d516e4a27d940c7dcc54f88bd4b66934112618
Fred Brunner <frdbrunner@gmail.com>
2025-05-20 15:57:01 +0200

    Trying to fix SSL again

--- start-with-ssl.js
+++ start-with-ssl.js
@@ -1,9 +1,15 @@
 process.env.DB_TYPE = 'postgresdb';
 process.env.DB_POSTGRESDB_SSL_REJECT_UNAUTHORIZED = 'false';
+process.env.DB_POSTGRESDB_SSL = JSON.stringify({ rejectUnauthorized: false });
 
-// Add default fallback for older environments
-if (!process.env.DB_POSTGRESDB_SSL) {
-  process.env.DB_POSTGRESDB_SSL = JSON.stringify({ rejectUnauthorized: false });
-}
+console.log('📡 Connecting to DB with:');
+console.log({
+  DB_HOST: process.env.DB_POSTGRESDB_HOST,
+  DB_PORT: process.env.DB_POSTGRESDB_PORT,
+  DB_NAME: process.env.DB_POSTGRESDB_DATABASE,
+  DB_USER: process.env.DB_POSTGRESDB_USER,
+  SSL: process.env.DB_POSTGRESDB_SSL,
+});
 
 require('./packages/cli/bin/n8n');
+
